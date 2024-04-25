b5a4b342145d6e2e5f96bdeba1fc3533cf42e4fc
lord-cepo <alessandro.ciavatta2@studenti.unibo.it>
2024-04-25 17:17:29 +0200

    more freedom for the user

--- runners/relax.py
+++ runners/relax.py
@@ -1,6 +1,6 @@
 from pwpy import pw2py, py2pw
 import bash
-
+import os
 ###########################################
 # -----------------------------------------
 INPUT_FILE = 'pw.in'
@@ -11,15 +11,19 @@ def relax(input_file=INPUT_FILE):
     d = pw2py(input_file)
     
     d['calculation'] = 'relax'
-    d['conv_thr'] = 1e-9
+    # d['conv_thr'] = 1e-9
     d['ion_dynamics'] = 'bfgs'
-    d['forc_conv_thr'] = 1e-4
-    d['etot_conv_thr'] = 1e-5
+    if 'forc_conv_thr' not in d: 
+        d['forc_conv_thr'] = 1e-4
+    if 'etot_conv_thr' not in d:
+        d['etot_conv_thr'] = 1e-5
     print("RELAX")
     py2pw(d, 'relax.pw.in')
     bash.srun('relax.pw.in')
 
     bash.run('last_coord.py relax.pw.out') #TODO: import this script directly
+    d['ATOMIC_POSITIONS'] = pw2py('relax.pw.in')['ATOMIC_POSITIONS']
+    os.remove('relax.pw.in')
     del d['ion_dynamics']
     
     py2pw(d, input_file)
