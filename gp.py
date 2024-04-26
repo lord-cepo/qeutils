dc2f9bb53076af50369641e8b2291836e8821a10
lord-cepo <alessandro.ciavatta2@studenti.unibo.it>
2024-04-26 15:57:24 +0200

    q2r + matdyn plotter

--- gp.py
+++ gp.py
@@ -1,46 +1,40 @@
+#!/usr/bin/env python
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+import os
 
-LOTO_000 = np.array([0, 0, 0, 37.64, 37.64, 52.95, 52.95, 61.32, 93.74, 93.74, 99.55, 100.09, 100.09, 119.2, 132.94])
-LOTO_100 = np.array([0, 0, 0, 37.64, 37.64, 52.95, 61.3, 74.1, 93.7, 94.5, 99.6, 100.1, 100.1, 119.2, 132.9])
-LOTO_111 = np.array([0, 0, 0, 37.64, 37.64, 52.95, 61.32, 72.59, 93.74, 94.3, 100.09, 100.09, 100.16, 119.29, 132.94])
-spacing = 100
-prefixes = ["epw-new", "old"]
-vlines = ["$\\Gamma$", "Z", "F", "$\\Gamma$", "L"]
-colors = ["green", "blue", "blue", "black"]
-# vlines = ['$\\Gamma$', 'T', 'H2|H0', 'L', '$\\Gamma$', 'S0|S2', 'F', '$\\Gamma$']
-xvlines = [ i*spacing for i in range(len(vlines)) ]
-# for i, lab in enumerate(vlines):
-#     if "|" in lab
-#         for j in range(i, len(vlines)):
-#             xvticks[j] += 1
-#         xvlines = xvlines[:i] + [xvlines[i]] + xvlines[i:]
-#         for j in range(i+1, len(xvlines)):
-#             xvlines[j] += 1
+# os.chdir('/linkhome/rech/genimp01/unr46rr/work/bi2te3/thirdorder')
 
-for prefix, color in zip(prefixes, colors):
-    data = np.loadtxt(prefix + '.gp')
-    to_remove = []
-    for i in range(data.shape[0]-1):
-        if data[i+1, 0] - data[i, 0] > 0.1:
-            to_remove.append(i)
-    data = np.delete(data, to_remove, axis=0)
-    xrange = np.linspace(0, 1, data.shape[0])
-    for i in range(1, data.shape[1]):
-        plt.plot(xrange, data[:, i], color=color, label=prefix if i == 1 else None)
-    # for i in range(15):
-    #     plt.plot(xrange[xvlines[3]], LOTO_100[i], 'o', color="black", label='100' if i == 1 else None)
-    #     plt.plot(xrange[xvlines[0]], LOTO_111[i], 'o', color="blue", label='111' if i == 1 else None)
+kpath = 'KPATH'
+pre_kpath = './'
+max_depth = 5
+counter = 0
+while kpath not in os.listdir(pre_kpath):
+    pre_kpath += '../'
+    counter += 1
+    if counter == 5:
+        raise("I did not find KPATH")
+    
+kpath = pre_kpath + kpath
+with open(kpath) as f:
+    lines = f.readlines()
 
+POINT_LABELS = [line.split('!')[1].strip() for line in lines if len(line.split('!')) == 2]
+for gpfile, color in zip([file for file in os.listdir() if file.endswith('.gp')], matplotlib.rcParams['axes.prop_cycle']):
+    data = np.loadtxt(gpfile)
+    X = data[:,0]
+    l = gpfile.replace('.gp', '')
+    for Y in data.T[1:,:]:
+        plt.plot(X,Y, color=color['color'], label=l)
+        l = None
 
-# data = np.loadtxt('epw.gp1')
-# for i in range(5, 22):
-#     plt.plot(data[:,0], data[:, i], '.', color="black")
-for xv in xvlines:
-    plt.axvline(x=xrange[xv], color="black")
+ticks = [X[i] for i in range(0,data.shape[0], data.shape[0]//(len(POINT_LABELS)-1))]
+for x in ticks:
+    plt.axvline(x, linestyle='--', color='grey')
+plt.xticks(ticks, POINT_LABELS)
 
-plt.xticks([xrange[xv] for xv in xvlines], vlines)
 plt.legend()
+plt.ylabel('energy [cm-1]')
 plt.tight_layout()
-# plt.savefig('bi2te3-soc.png', dpi=500)
-plt.show()
+plt.savefig('gp.png', dpi=500)
